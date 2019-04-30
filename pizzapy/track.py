bd0628118af84f91b50a16c9636b5db0a874a58d
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-04-30 16:37:44 -0400

    Create track.py

---
+++ pizzapy/track.py
@@ -0,0 +1,27 @@
+from .urls import Urls, COUNTRY_USA
+from .utils import request_xml, request_json
+
+
+def track_by_phone(phone, country=COUNTRY_USA):
+    """Query the API to get tracking information.
+    Not quite sure what this gets you - problem to solve for next time I get pizza. 
+    """
+    phone = str(phone).strip()
+    data = request_xml(
+        Urls(country).track_by_phone(), 
+        phone=phone
+    )['soap:Envelope']['soap:Body']
+
+    response = data['GetTrackerDataResponse']['OrderStatuses']['OrderStatus']
+
+    return response
+
+
+def track_by_order(store_id, order_key, country=COUNTRY_USA):
+    """Query the API to get tracking information.
+    """
+    return request_json(
+        Urls(country).track_by_order(),
+        store_id=store_id,
+        order_key=order_key
+    )
