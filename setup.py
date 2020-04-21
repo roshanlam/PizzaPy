8974e324c054ba1a8247e8a93228a5199418e0a3
Roshan Lamichhane <lamichhaner40@gmail.com>
2020-04-21 16:49:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.0.1',
 
     description='A Python Program to Pizza.',
     long_description=long_description,
@@ -73,31 +73,15 @@ setup(
     # The project's main homepage.
     url='https://github.com/roshancode/PizzaPy',
 
-    # Author details
     author='Roshan Lamichhhane',
     author_email='roshanlamichhanenepali@gmail.com',
-    author_website='roshanlamichhane.com',
-
-    # What does your project relate to?
     keywords='Order Pizza',
-
-    # Choose your license
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # TODO: Add testing/support for more python versions
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python',
         # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -109,25 +93,7 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['tests']),
-
-    # TODO: Add a command line tool
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'pizzapy=pizzapy:main'
-    #     ],
-    # },
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'pyhamcrest',
         'requests', 
@@ -138,8 +104,6 @@ setup(
         'mock',
         'pytest',
     ],
-
-    # setup.py publish support.
     cmdclass={
         'publish': PublishCommand,
     },
