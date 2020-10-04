2bbfe14c538fadadeab152119c3fa7aea1e4bab6
Roshan Lamichhane <lamichhaner40@gmail.com>
2020-10-04 11:15:16 -0400

    Create main.py

---
+++ pizzapy/voice/abilities/general/main.py
@@ -0,0 +1,28 @@
+from random import choice, randint
+
+greetings = ['hey', 'hi', 'hello']
+status = ['amazing', 'awesome', 'I am great today! How are you doing today?']
+thank = ['you are welcome', 'all good']
+
+def greeting(AI, command):
+	AI.speak(choice(greetings))
+
+# how are you
+def hru(AI, command):
+	AI.speak(choice(status))
+
+def thanks(AI, command):
+	AI.speak(choice(thank))
+
+def repeat(AI, command):
+	try:
+		with open(AI.cachePath + 'last.txt', 'r') as file:
+			last = file.readline()
+			if last == 'failed':
+				AI.speak('Your last command failed')
+			elif last:
+				exec(last)
+			else:
+				AI.speak('You have not said anything yet :(')
+		except:
+			AI.speak('You have not said anything yet')
